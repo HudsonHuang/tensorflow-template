04801d6c5637ad293fe977b9b1009a1be02f3eef
790209714@qq.com <790209714@qq.com>
2017-12-27 20:19:48 +0800

    Added hparams.py for hyper-parameters defination

--- models/model_example.py
+++ models/model_example.py
@@ -7,21 +7,21 @@ Created on Sun Dec  3 22:47:44 2017
 import tensorflow as tf
 
 class model_example(object):
-    def __init__(self, len_x=1, n_layers=1):  
+    def __init__(self, hp,len_x=1, n_layers=1):  
         with tf.name_scope("model_example"): 
-              self.x = tf.placeholder(tf.float32, [None, 784])
-              W = tf.Variable(tf.zeros([784, 10]))
-              b = tf.Variable(tf.zeros([10]))
+              self.x = tf.placeholder(tf.float32, [None, hp.input_dim])
+              W = tf.Variable(tf.zeros([hp.input_dim, hp.output_dim]))
+              b = tf.Variable(tf.zeros([hp.output_dim]))
               self.y = tf.matmul(self.x, W) + b
               
               #use For loop to allocate more layers
               #Or any conditional branch to make computational graph dynamically
-              for i in range(n_layers):
-                  self.y=model_example.more_layers(self.y)
+              for i in range(hp.n_layers):
+                  self.y=model_example.more_layers(self.y,hp.output_dim)
               
             
               # Define loss and optimizer
-              self.y_ = tf.placeholder(tf.float32, [None, 10])
+              self.y_ = tf.placeholder(tf.float32, [None, hp.output_dim])
             
               # The raw formulation of cross-entropy,
               #
@@ -35,12 +35,12 @@ class model_example(object):
               self.cross_entropy = tf.reduce_mean(
                   tf.nn.softmax_cross_entropy_with_logits(
                           labels=self.y_, logits=self.y))
-              self.train_step = tf.train.GradientDescentOptimizer(0.5).minimize(
+              self.train_step = tf.train.GradientDescentOptimizer(hp.lr).minimize(
                       self.cross_entropy)
 
-    def more_layers(y):
-        W = tf.Variable(tf.zeros([10]))
-        b = tf.Variable(tf.zeros([10]))
+    def more_layers(y,num_units):
+        W = tf.Variable(tf.zeros([num_units]))
+        b = tf.Variable(tf.zeros([num_units]))
         y = y*W + b
         y = tf.nn.relu(y)
         return y

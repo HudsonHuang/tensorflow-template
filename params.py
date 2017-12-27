6277486816f1c4e88668d4b43e41c9aec274204f
790209714@qq.com <790209714@qq.com>
2017-12-28 00:11:14 +0800

    Added modelSelection in main.py

---
+++ params.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+#/usr/bin/python2
+
+# path
+## local
+data_path_base = './datasets'
+logdir_path = './logdir'
+
+## remote
+# data_path_base = '/data/private/vc/datasets'
+# logdir_path = '/data/private/vc/logdir'
+
+# hyper_params is about experiment
+class default_hyper_params:
+    num_epochs = 1000
+    eval_per_epoch = num_epochs//10 +1 # 10 times for eval in total train
+    save_per_epoch = num_epochs//2 +1  # floor_division + 1 == ceiling_division
+    batch_size = 32
+
+# model_params is about model structure
+class MLP_model_params:
+    # data
+    input_dim = 784
+    output_dim = 10
+
+
+    # model
+    hidden_units = 256  # alias = E
+    n_layers = 1
+    num_banks = 16
+    dropout_rate = 0.2
+
+    # train
+    batch_size = 32
+    lr = 0.001
+
+
+class Test1_params:
+    # path
+    data_path = '{}/timit/TIMIT/TEST/*/*/*.wav'.format(data_path_base)
+
+    # test
+    batch_size = 32
+
+
+class Convert_params:
+    # path
+    data_path = '{}/arctic/bdl/*.wav'.format(data_path_base)
+
+    # convert
+    batch_size = 2
+    emphasis_magnitude = 1.2

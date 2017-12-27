79fd889d4163234aa2045dba5bb3643596403012
790209714@qq.com <790209714@qq.com>
2017-12-28 00:33:57 +0800

    Renew README.md

--- Minist_example.sh
+++ Minist_example.sh
@@ -1,5 +1,3 @@
 #path or other
-echo "To checkout tensorboard, run `tensorboard --logdir=./logdir/experiment_name1`"
 python prepare_features.py
 python main.py
-echo "Please find your results in `./generated/experiment_name1`"
